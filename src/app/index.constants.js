46bf52ba0b29061c2af0b4623aa0e4ae9fd52462
hotkongzaa <hotkong.1993@gmail.com>
2016-07-18 22:09:37 +0700

    adding initial project

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('realestate')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
